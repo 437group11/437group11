a4cf07b7080b3fefefc9f09a4d1fed9be178587c
NoahCCB <c.noah@wustl.edu>
2023-10-01 16:10:33 -0500

    added auth

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [], //could fill iwth AppController
+  providers: [], //could fill with AppServie
 })
 export class AppModule {}
 
