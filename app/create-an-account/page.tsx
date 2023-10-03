96dbea62f39de766b7147f0fcd309c6cfc80e794
Fisher Sun <f.sun@wustl.edu>
2023-10-02 21:30:37 -0500

    Use type password

--- app/create-an-account/page.tsx
+++ app/create-an-account/page.tsx
@@ -22,14 +22,14 @@ export default function CreateAnAccount() {
                     <label className="block">
                         Password:
                     </label>
-                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                    <input className="rounded-sm border-gray-300 border-2" type="password" required />
                 </div>
 
                 <div className="my-4">
                     <label className="block">
                         Confirm password:
                     </label>
-                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                    <input className="rounded-sm border-gray-300 border-2" type="password" required />
                 </div>
 
                 <button type="submit" className="p-4 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Create</button>
