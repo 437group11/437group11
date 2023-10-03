318f54761d74b2020f821b961e50107224e72493
a-kirsch <a.kirsch@wustl.edu>
2023-10-03 14:43:37 -0500

    route to feed

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,3 +1,5 @@
+import ButtonLink from "../../components/button-link";
+
 export default function SignIn() {
     return (
         <>
@@ -17,8 +19,9 @@ export default function SignIn() {
                     </label>
                     <input className="rounded-sm border-gray-300 border-2" type="password" required />
                 </div>
-
-                <button type="submit" className="p-4 bg-accent-brown text-white py-2 rounded-md hover:bg-accent-brown-darker focus:outline-none focus:ring-4 focus:ring-accent-blue">Sign in</button>
+                <ButtonLink href="/feed">
+                    Log in
+                </ButtonLink>
             </form>
         </>
     )
