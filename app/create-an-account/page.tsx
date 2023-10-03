a5901b504a1de88b7468b93d8456ce840374a658
Fisher Sun <f.sun@wustl.edu>
2023-10-02 21:28:17 -0500

    Add form

--- app/create-an-account/page.tsx
+++ app/create-an-account/page.tsx
@@ -2,6 +2,38 @@ export default function CreateAnAccount() {
     return (
         <>
             <h1>Create an account</h1>
+
+            <form className="mt-8">
+                <div className="mb-4">
+                    <label className="block">
+                        Username:
+                    </label>
+                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                </div>
+
+                <div className="my-4">
+                    <label className="block">
+                        Email:
+                    </label>
+                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                </div>
+
+                <div className="my-4">
+                    <label className="block">
+                        Password:
+                    </label>
+                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                </div>
+
+                <div className="my-4">
+                    <label className="block">
+                        Confirm password:
+                    </label>
+                    <input className="rounded-sm border-gray-300 border-2" type="text" required />
+                </div>
+
+                <button type="submit" className="p-4 bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Create</button>
+            </form>
         </>
     )
 }
