97f9fa8047d4dc1bcf2fe1ee950d07bdbd1fa2db
a-kirsch <a.kirsch@wustl.edu>
2023-10-03 13:00:16 -0500

    routing testing

--- app/create-an-account/page.tsx
+++ app/create-an-account/page.tsx
@@ -24,7 +24,6 @@ export default function CreateAnAccount() {
                     </label>
                     <input className="rounded-sm border-gray-300 border-2" type="password" required />
                 </div>
-
                 <button type="submit" className="p-4 bg-accent-brown text-white py-2 rounded-md hover:bg-accent-brown-darker focus:outline-none focus:ring-4 focus:ring-accent-blue">Create</button>
             </form>
         </>
