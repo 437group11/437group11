7fd9db98f722032c0a880378218075de527f348b
Fisher Sun <f.sun@wustl.edu>
2023-10-16 22:51:01 -0500

    Add next-auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,17 @@
 /**
  * Overrides the default app to apply global styles.
  * https://nextjs.org/docs/pages/building-your-application/styling/css-modules#global-styles
+ *
+ * Additionally, sets up shared session state for next-auth.
+ * https://next-auth.js.org/getting-started/example#configure-shared-session-state
  */
 import '../styles/globals.css'
+import {SessionProvider} from "next-auth/react"
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default function App({Component, pageProps: {session, ...pageProps}}) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps}/>
+        </SessionProvider>
+    )
 }
