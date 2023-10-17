ecbab5e619c790f8a297e2eca773f92f280abbc7
Fisher Sun <f.sun@wustl.edu>
2023-10-16 23:19:03 -0500

    Revert "Add next-auth"
    This reverts commit 7fd9db98f722032c0a880378218075de527f348b.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,10 @@
 /**
  * Overrides the default app to apply global styles.
  * https://nextjs.org/docs/pages/building-your-application/styling/css-modules#global-styles
- *
- * Additionally, sets up shared session state for next-auth.
- * https://next-auth.js.org/getting-started/example#configure-shared-session-state
  */
 import '../styles/globals.css'
-import {SessionProvider} from "next-auth/react"
 
-export default function App({Component, pageProps: {session, ...pageProps}}) {
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps}/>
-        </SessionProvider>
-    )
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
