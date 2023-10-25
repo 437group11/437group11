b01d8cef09fcc21c9a5acd38fc11f9fe2e6780f2
Fisher Sun <f.sun@wustl.edu>
2023-10-24 19:14:56 -0500

    Add Chakra

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,9 +2,14 @@
  * Overrides the default app to apply global styles.
  * https://nextjs.org/docs/pages/building-your-application/styling/css-modules#global-styles
  */
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    )
 }
