7829c990d17b1c18daeaa7cd8a5580b4953e46e7
Fisher Sun <f.sun@wustl.edu>
2023-10-27 01:29:52 -0500

    Take 10 users when searching

---
+++ pages/api/v1/users.ts
@@ -0,0 +1,71 @@
+/**
+ * This route allows searching for a user by passing the GET parameter ?username=
+ */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Prisma } from "@prisma/client"
+import prisma from "utils/db"
+import { HttpStatusCode } from 'axios'
+
+async function searchUserPublicData(username: string) {
+    return await prisma.user.findMany({
+        where: {
+            username: {
+                contains: username
+            }
+        },
+        select: {
+            username: true,
+            dateRegistered: true
+        },
+        take: 10
+    })
+}
+
+/**
+ * The return type of the publicly accessible information.
+ */
+export type UserPublicDataArray = Prisma.PromiseReturnType<typeof searchUserPublicData>
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { username } = req.query
+
+    function isString(s: string | string[] | undefined): s is string {
+        return typeof s === "string";
+    }
+    if (!isString(username)) {
+        res.status(400).json({
+            "status": "fail",
+            "data": {
+                "title": "Username query must be a string"
+            }
+        })
+        return
+    }
+
+    const users = await searchUserPublicData(username)
+        .catch((error) => {
+            if ("message" in error) {
+                res.status(HttpStatusCode.InternalServerError).json({
+                    "status": "fail",
+                    "data": {
+                        "title": error.message
+                    }
+                })
+                return
+            } else {
+                res.status(HttpStatusCode.InternalServerError).json({
+                    "status": "fail",
+                    "data": {
+                        "title": "An unknown error occurred."
+                    }
+                })
+                return
+            }
+            
+        })
+
+    res.status(HttpStatusCode.Ok).json({
+        "status": "success",
+        "users": users
+    })
+}
