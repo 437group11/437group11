612aaa79faf5c52aa1ae856175d6aaf53c955947
Fisher Sun <f.sun@wustl.edu>
2023-11-03 03:21:52 -0500

    Reduce code duplication in header

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { getUsername } from "utils/userManager";
 import { useSession, signIn, signOut } from "next-auth/react";
-import { Button } from "@chakra-ui/react";
+import { Button, Heading } from "@chakra-ui/react";
 
 export default function Header() {
     const { data: session, status } = useSession();
@@ -12,42 +12,27 @@ export default function Header() {
     function headerContents(): JSX.Element {
         if (!session) {
             // Not signed in: return basic header
-            return (
-                <>
-                    <Link href="/">
-                            Beatbuff
-                    </Link>
-                </>
-            )
+            return <></>
         }
         
         if (pathname === `/profile/[id]`) {
             // Signed in and profile page: add button to sign out
-            return (
-                <>
-                    <Link href="/feed">
-                        Beatbuff
-                    </Link>
-                    <Button onClick={() => signOut()} colorScheme="red">Sign out</Button>
-                </>
-            )
+            return <Button onClick={() => signOut()} colorScheme="red">Sign out</Button>
         }
+        
         // Otherwise, add link to profile
-        return (
-            <>
-                <Link href="/feed">
-                    Beatbuff
-                </Link>
-
-                <Button onClick={() => router.push(`/profile/${session.user?.id}`)}>Profile</Button>
-            </>
-        )
+        return <Button onClick={() => router.push(`/profile/${session.user?.id}`)}>Profile</Button>
     }
     
     let contents = headerContents()
 
     return (
         <header className="bg-bg-darker p-8 flex justify-between">
+            <Heading>
+                <Link href="/">
+                    Beatbuff
+                </Link>
+            </Heading>
             {contents}
         </header>
     )
