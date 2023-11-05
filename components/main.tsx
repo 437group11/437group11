def17748775afc66fe1c7cf689a9f04b26f722c2
Fisher Sun <f.sun@wustl.edu>
2023-11-04 21:48:36 -0500

    Make main take up at least a full screen
    Prevents ugly white background

--- components/main.tsx
+++ components/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import { Container } from "@chakra-ui/react";
 export default function main({ children }: {children: React.ReactNode}) {
-    return <main className="w-screen bg-bg">{children}</main>
+    return <main className="w-screen min-h-screen bg-bg">{children}</main>
 }
