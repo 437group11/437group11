a2b11eabca7ebbf83b7f1f6ca8d61dbd73b225bc
Fisher Sun <f.sun@wustl.edu>
2023-11-07 09:31:00 -0600

    Format root-layout.tsx

--- components/root-layout.tsx
+++ components/root-layout.tsx
@@ -1,35 +1,30 @@
-import type { Metadata } from 'next'
-import { Lora } from 'next/font/google'
+import type { Metadata } from "next"
+import { Lora } from "next/font/google"
 import Header from "./header"
 import Main from "./main"
-import React from "react";
-import Head from "next/head";
+import React from "react"
+import Head from "next/head"
 
-const myFont = Lora({ subsets: ['latin'] })
+const myFont = Lora({ subsets: ["latin"] })
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-      <>
-          {/*
+    return (
+        <>
+            {/*
           The head component allows us to append elements to <head> from the body.
           https://nextjs.org/docs/pages/api-reference/components/head
           */}
-          <Head>
-              <title>Beatbuff</title>
-              <meta 
-                name="description"
-                content="Rate and share albums"
-              />
-          </Head>
+            <Head>
+                <title>Beatbuff</title>
+                <meta name="description" content="Rate and share albums" />
+            </Head>
 
-          <Header></Header>
-          <Main>
-              {children}
-          </Main>
-      </>
-  )
+            <Header></Header>
+            <Main>{children}</Main>
+        </>
+    )
 }
