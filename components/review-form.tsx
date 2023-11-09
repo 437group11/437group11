cd7dfa535d68d4ad3ae5534d63bc727ae1a6aff2
Fisher Sun <f.sun@wustl.edu>
2023-11-09 17:36:55 -0600

    Add labels to form

--- components/review-form.tsx
+++ components/review-form.tsx
@@ -1,10 +1,11 @@
-import { Button, FormControl, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from "@chakra-ui/react"
+import { Button, FormControl, FormLabel, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from "@chakra-ui/react"
 import React from "react"
 
 export default function ReviewForm() {
     return (
-        <>
+        <form>
             <FormControl>
+                <FormLabel>Rating 1-10</FormLabel>
                 <NumberInput max={10} min={0} step={0.1}>
                     <NumberInputField />
                     <NumberInputStepper>
@@ -12,11 +13,14 @@ export default function ReviewForm() {
                         <NumberDecrementStepper />
                     </NumberInputStepper>
                 </NumberInput>
-                
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Review text</FormLabel>
                 <Textarea maxLength={191}></Textarea>
             </FormControl>
 
             <Button>Add a review</Button>
-        </>
+        </form>
     )
 }
