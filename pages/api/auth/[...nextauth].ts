2f690a65955c8b18682511b0a287bd3a73da7b5f
Fisher Sun <f.sun@wustl.edu>
2023-11-25 18:25:59 -0500

    Explicitly identify Knock recipient

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
 import SpotifyProvider from "next-auth/providers/spotify"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import { Knock } from "@knocklabs/node"
 import prisma from "utils/db"
 
 export const authOptions = {
@@ -38,10 +39,18 @@ export const authOptions = {
             }
             return true
         },
-        // https://github.com/t3-oss/create-t3-app/issues/176
         async session({ session, user }: {session: Session, user: User}) {
+            // https://github.com/t3-oss/create-t3-app/issues/176
             session.user.id = user.id;
             session.spotifyToken = await requestSpotifyAccessToken()
+            
+            // https://docs.knock.app/managing-recipients/identifying-recipients#explicitly-identifying-recipients
+            const knock = new Knock(process.env.KNOCK_SECRET_KEY!)
+            knock.users.identify(user.id, {
+                name: user.name ?? undefined,
+                email: user.email ?? undefined
+            })
+
             return session
         },
       }
