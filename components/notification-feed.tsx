d3af865c91e1c4dedcbf43d62a4daeecb2b2bfe5
Fisher Sun <f.sun@wustl.edu>
2023-11-25 17:15:43 -0500

    Use Chakra BellIcon for notification feed

--- components/notification-feed.tsx
+++ components/notification-feed.tsx
@@ -9,6 +9,8 @@ import { Session } from "next-auth"
 
 // Required CSS import, unless you're overriding the styling
 import "@knocklabs/react-notification-feed/dist/index.css"
+import { IconButton } from "@chakra-ui/react"
+import { BellIcon } from "@chakra-ui/icons"
 
 export default function NotificationFeed({ session }: {session: Session}): JSX.Element {
     const [isVisible, setIsVisible] = useState(false)
@@ -17,8 +19,10 @@ export default function NotificationFeed({ session }: {session: Session}): JSX.E
     return (
         <KnockFeedProvider apiKey={process.env.KNOCK_PUBLIC_KEY!} userId={session.user.id} feedId={process.env.KNOCK_FEED_CHANNEL_ID!}>
             <>
-                <NotificationIconButton
+                <IconButton
                     ref={notifButtonRef}
+                    icon={<BellIcon />}
+                    aria-label="Show notifications feed"
                     onClick={(e) => setIsVisible(!isVisible)}
                 />
                 <NotificationFeedPopover
