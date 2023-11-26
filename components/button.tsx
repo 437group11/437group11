e4e4a3a87b600b138ba79340ecfe513329d84437
Fisher Sun <f.sun@wustl.edu>
2023-11-25 21:38:57 -0500

    Remove old components

--- components/button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ children, type }: { children: React.ReactNode, type: any }) {
-    return (
-        <button type={type} className="p-4 bg-header text-white py-2 rounded-md hover:bg-accent-brown-darker focus:outline-none focus:ring-4 focus:ring-accent-blue">
-            {children}
-        </button>
-    )
-}
