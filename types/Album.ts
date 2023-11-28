9edd98f480621c66c7940dc2d09ff3c0159b9115
Fisher Sun <f.sun@wustl.edu>
2023-11-27 21:34:12 -0600

    Album artists string[] -> Artist[]

--- types/Album.ts
+++ types/Album.ts
@@ -1,9 +1,10 @@
 import internal from "stream";
+import { Artist } from "./Artist";
 
 export interface Album {
     id: string;
     name: string;
-    artists: string[];
+    artists: Artist[];
     release_date?: string;
     type?: string;
     image : string;
